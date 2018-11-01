4ce38ab33ca7631a55be8165aa5288181431e06c
Austin Miles <abm@austinbmiles.com>
2018-11-01 14:56:39 -0400

    moved hashtag definition into hashtags controller from searchquery model

--- app/queries/shout_search_query.rb
+++ app/queries/shout_search_query.rb
@@ -6,6 +6,6 @@ class ShoutSearchQuery
 
   def to_relation
     Shout.joins("LEFT JOIN text_shouts ON content_type = 'TextShout' AND content_id = text_shouts.id")
-                    .where("text_shouts.body LIKE ?", "%##{term}%")
+                    .where("text_shouts.body LIKE ?", "%#{term}%")
   end
 end
