7aa4f0f3ef77e28896bd3cffbc6ca5b81651adae
Austin Miles <abm@austinbmiles.com>
2018-11-01 14:45:15 -0400

    pulled out query into a service

--- app/controllers/hashtags_controller.rb
+++ app/controllers/hashtags_controller.rb
@@ -1,6 +1,9 @@
 class HashtagsController < ApplicationController
   def show
-    @hashtag = params[:id]
-    @results = Shout.search(term: @hashtag)
+    @search = SearchService.new(term:hashtag)
+  end
+
+  def hashtag
+    params[:id]
   end
 end
--- app/models/shout.rb
+++ app/models/shout.rb
@@ -4,8 +4,4 @@ class Shout < ApplicationRecord
   validates :user, presence: true
 
   delegate :username, to: :user
-
-  scope :search, ->(term:'') { joins("LEFT JOIN text_shouts ON content_type = 'TextShout' AND content_id = text_shouts.id")
-                                   .where("text_shouts.body LIKE ?", "%##{term}%")}
-
 end
